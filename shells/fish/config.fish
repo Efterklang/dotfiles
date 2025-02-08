faf01f039d51f6ef68dd5ceee5d9ab3c6322a8c2
Efterklang <113397634+Efterklang@users.noreply.github.com>
2025-02-08 13:13:19 +0800

    feat(fish): add visual env var and more alias

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -4,13 +4,16 @@ end
 
 # envirment variables
 set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx VISUAL helix  # or nvim
 # cli tools
 zoxide init fish | source
 oh-my-posh init fish --config ~/.config/ohmyposh/omp.json | source
 
 # alias
+alias cls="clear"
 alias f="fish"
 alias gg="gitui"
 alias lg="lazygit"
 alias vim="nvim"
 alias cd="z"
+alias cat="bat"
