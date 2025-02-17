011f7ae468d5bc5c4ac03e2c5cf5dda33eb0e346
jiaxing.gao <jiaxing.gao@momenta.ai>
2025-02-17 11:44:41 +0800

    feat(nvim): using OSC 52 protocol to access system clipboard

--- tui_cli/nvim/lua/config/autocmds.lua
+++ tui_cli/nvim/lua/config/autocmds.lua
@@ -1,15 +1,28 @@
-if vim.fn.executable("win32yank.exe") == 1 then
-  vim.opt.clipboard:append({ "unnamedplus" })
-  vim.g.clipboard = {
-    name = "win32yank",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-  }
+function my_paste(reg)
+    return function(lines)
+        --[ 返回 “” 寄存器的内容，用来作为 p 操作符的粘贴物 ]
+        local content = vim.fn.getreg('"')
+        return vim.split(content, '\n')
+    end
 end
+
+if (os.getenv('SSH_TTY') == nil)
+then
+    --[ 当前环境为本地环境，也包括 wsl ]
+    vim.opt.clipboard:append("unnamedplus")
+else
+    vim.opt.clipboard:append("unnamedplus")
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+      },
+      paste = {
+        --[ 小括号里面的内容可能是毫无意义的，但是保持原样可能看起来更好一点 ]
+        ["+"] = my_paste("+"),
+        ["*"] = my_paste("*"),
+    },
+}
+end
+
