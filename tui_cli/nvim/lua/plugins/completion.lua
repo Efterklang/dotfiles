6e051dd298273128d36d73c24e3bb606c57d7540
Efterklang <gaojiaxing2004@qq.com>
2025-09-23 01:12:36 +0800

    feat(nvim): setup blink.cmp and set vim.o.winborder to rounded

---
+++ tui_cli/nvim/lua/plugins/completion.lua
@@ -0,0 +1,48 @@
+return {
+  "saghen/blink.cmp",
+  event = { "BufReadPost", "BufNewFile" },
+  version = "1.*",
+  dependencies = { "xzbdmw/colorful-menu.nvim", opts = {} },
+  opts = {
+    completion = {
+      menu = {
+        draw = {
+          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require("colorful-menu").blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require("colorful-menu").blink_components_highlight(ctx)
+              end,
+            },
+          },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+      },
+    },
+    keymap = {
+      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+    },
+    signature = {
+      enabled = true,
+    },
+    cmdline = {
+      completion = {
+        menu = {
+          auto_show = true,
+        },
+      },
+    },
+    sources = {
+      providers = {
+        snippets = { score_offset = 1000 },
+      },
+    },
+  },
+}
