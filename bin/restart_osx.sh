c48271ca0d20c07491753fa18256e1fbbad4baee
Efterklang <gaojiaxing2004@qq.com>
2025-10-19 18:08:03 +0800

    feat(bin): add new env_vars; update restart_osx scirpt(now no need for sudo)

--- bin/restart_osx.sh
+++ bin/restart_osx.sh
@@ -12,18 +12,6 @@
 # @raycast.author gnixaij_oag
 # @raycast.authorURL https://raycast.com/gnixaij_oag
 
-# 关闭 Ghostty 应用
-echo "关闭 Ghostty..."
 osascript -e 'tell application "Ghostty" to quit' 2>/dev/null
-
-# 关闭 Visual Studio Code 应用
-echo "关闭 Visual Studio Code..."
 osascript -e 'tell application "Visual Studio Code" to quit' 2>/dev/null
-
-# 等待应用关闭
-echo "等待应用关闭..."
-sleep 5
-
-# 重启电脑
-echo "正在重启电脑..."
-sudo shutdown -r now
+osascript -e 'tell app "System Events" to restart'
