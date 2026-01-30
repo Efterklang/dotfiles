6a5576484082ed31b5b436f7b19aa1c1682f7ee1
Efterklang <gaojiaxing2004@qq.com>
2026-01-30 23:09:12 +0800

    feat(nvim): add injected formatter support for markdown code blocks
    - Configure injected formatter to format embedded code blocks
    - Refactor formatters config to reduce duplication
    - Reuse formatters table for both formatters_by_ft and lang_to_formatters
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- tui_cli/nvim/lua/plugins/conform.lua
+++ tui_cli/nvim/lua/plugins/conform.lua
@@ -3,29 +3,40 @@ return {
     "stevearc/conform.nvim",
     event = "BufWritePre",
     opts = function()
+      local sh = { "shfmt" }
+      local biome = { "biome" }
+
+      local formatters = {
+        bash = sh,
+        sh = sh,
+        zsh = sh,
+        lua = { "stylua" },
+        go = { "goimports" },
+        python = { "ruff_format" },
+        javascript = biome,
+        typescript = biome,
+        javascriptreact = biome,
+        typescriptreact = biome,
+        json = biome,
+        jsonc = biome,
+        yaml = biome,
+        html = biome,
+        css = biome,
+        less = biome,
+        sass = biome,
+      }
+
       return {
-        formatters_by_ft = {
-          bash = { "shfmt" },
-          sh = { "shfmt" },
-          zsh = { "shfmt" },
-          nu = { "nufmt" },
-          lua = { "stylua" },
-          go = { "goimports" },
-          python = { "ruff_format" },
-
-          javascript = { "biome" },
-          typescript = { "biome" },
-          javascriptreact = { "biome" },
-          typescriptreact = { "biome" },
-
-          json = { "biome" },
-          jsonc = { "biome" },
-          yaml = { "biome" },
-          html = { "biome" },
-
-          css = { "biome" },
-          less = { "biome" },
-          sass = { "biome" },
+        formatters_by_ft = vim.tbl_extend("force", formatters, {
+          markdown = { "injected" },
+        }),
+        formatters = {
+          injected = {
+            options = {
+              ignore_errors = true,
+              lang_to_formatters = formatters,
+            },
+          },
         },
       }
     end,
